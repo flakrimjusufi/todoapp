dba6671f9e6d7505976d1eda85f03d56f2290b5e
flakrim <flakrim.jusufi@gmail.com>
2021-10-27 16:55:20 +0200

    Added: UI that consumes the data from API

--- controllers/handler.go
+++ controllers/handler.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	"encoding/json"
+	"html/template"
 	"log"
 	"net/http"
 	"todoapp/samples"
@@ -19,4 +20,32 @@ func GetTodoList(w http.ResponseWriter, r *http.Request) {
 	if handleError != nil {
 		log.Fatalln(handleError)
 	}
+}
+
+func RenderTemplate(res http.ResponseWriter, req *http.Request) {
+
+	defer func() {
+		if e := recover(); e != nil {
+			log.Println(e)
+			res.WriteHeader(http.StatusInternalServerError)
+		}
+	}()
+
+	t, err := template.ParseFiles("templates/view.html")
+
+	sampleData := samples.Sample{}
+
+	got, err := sampleData.GetSampleData()
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	data := make(map[string]interface{})
+
+	data["toDoList"] = got
+
+	if err != nil {
+		log.Println(err)
+	}
+	_ = t.Execute(res, data)
 }
--- routes/routes.go
+++ routes/routes.go
@@ -2,14 +2,16 @@ package routes
 
 import (
 	"github.com/gorilla/mux"
+	"net/http"
 	"todoapp/controllers"
 )
 
 func HandleRoutes() *mux.Router {
 	route := mux.NewRouter()
 
+	route.HandleFunc("/", controllers.RenderTemplate)
 	route.HandleFunc("/getTodoList", controllers.GetTodoList).Methods("GET")
-	//route.HandleFunc("/", controllers.Show)
+	route.PathPrefix("/templates/").Handler(http.StripPrefix("/templates/", http.FileServer(http.Dir("templates"))))
 
 	return route
 }
